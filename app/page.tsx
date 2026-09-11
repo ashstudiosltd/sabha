b5feeeb1d5b70501a1053c147973c3a336bfbdd7
ashutoshkahlon <ashstudiosltd@gmail.com>
2026-09-11 21:34:08 +0530

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,15 @@
 
 import Nav from "@/app/sabha/nav";
 import Hero from "@/app/sabha/hero";
+import AuthGate from "./auth/authgate";
 
 export default function LokPage() {
   return (
     <div style={{  minHeight: "100vh" }}>
+      <AuthGate>
       <Nav />
       <Hero />
+      </AuthGate>
     </div>
   );
 }
