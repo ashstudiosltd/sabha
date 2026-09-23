23a7bb7d73b994fe7f9bf0caad27ffc1cbbea1ff
ashutoshkahlon <ashstudiosltd@gmail.com>
2026-09-24 00:40:36 +0530

    new footer

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from 'react'
 import BlogsPage from './blogspage';
-import Footer from '../sabha/footer';
+import Footer from './footer';
 import Nav from '../sabha/nav';
 const blog= () => {
   return (
